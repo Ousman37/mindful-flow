9ee3a7611a7d6e440caecea64e6a9d115e293ef3
Ethmane Didi Bilal <blueskytech@Ethmanes-MBP.home>
2024-08-13 21:40:27 +0200

    Refactor layout for improved navigation structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,36 @@
-import { Slot } from "expo-router";
+import TimerProvider from "@/context/TimerContext";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 
+// This will prevent the splash screen from hiding until the fonts are loaded
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-    return (
-        <Slot />
-        
-    )
+  const [fontsLoaded, error] = useFonts({
+    "Roboto-Mono": require("../assets/fonts/RobotoMono-Regular.ttf"),
+  });
+
+  // Load the fonts and hide the splash screen
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  // If the fonts are not loaded, return
+  if (!fontsLoaded && !error) return null;
+  return (
+    <TimerProvider>
+
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="meditate/[id]" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="(modal)/adjust-meditation-duration"
+          options={{ headerShown: false, presentation: "modal" }}
+        />
+      </Stack>
+    </TimerProvider>
+  );
 }
