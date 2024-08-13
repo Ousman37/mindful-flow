66c8db8a401546f0cbe596f9baf125d157002092
Ethmane Didi Bilal <blueskytech@Ethmanes-MBP.home>
2024-08-13 21:40:33 +0200

    Enhance index screen with UI/UX improvements

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import beachImage from "@/assets/meditation-images/beach.webp";
 import { View, Text, ImageBackground, SafeAreaView } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar } from "expo-status-bar";
+import CustomButton from "@/components/CustomButton";
+import beachImage from "@/assets/meditation-images/beach.webp";
+import { useRouter } from "expo-router";
+import AppGradient from "@/components/AppGradient";
 
 const App = () => {
+  const router = useRouter();
   console.log("Rendering App Component");
 
   return (
@@ -15,15 +19,10 @@ const App = () => {
         resizeMode="cover"
         className="flex-1"
       >
-        <LinearGradient
-          className="flex-1 justify-center items-center"
-          colors={["rgba(0, 0, 0, 0.4)", "rgba(0, 0, 0, 0.8)"]}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 1, y: 1 }}
-        >
-          <SafeAreaView className="flex-1 px-4 justify-between">
-            <View>
-              <Text className="font-bold text-white text-4xl text-center">
+        <AppGradient colors={["rgba(0, 0, 0, 0.4)", "rgba(0, 0, 0, 0.8)"]}>
+          <SafeAreaView className="flex-1 mx-5 px-1 justify-between">
+            <View className="items-center">
+              <Text className="font-bold text-white text-4xl text-center mb-2">
                 Mindful Flow
               </Text>
               <Text className="text-white text-2xl leading-relaxed text-center">
@@ -31,8 +30,16 @@ const App = () => {
                 moment of calm.
               </Text>
             </View>
+            <View>
+              <CustomButton
+                onPress={() => router.push("/nature-meditate")}
+                title="Get Started"
+                textStyle="text-lg font-semibold"
+                containerStyle="bg-primary mt-8"
+              />
+            </View>
           </SafeAreaView>
-        </LinearGradient>
+        </AppGradient>
       </ImageBackground>
     </View>
   );
