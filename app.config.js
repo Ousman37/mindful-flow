db080a2659bb435baad5f8cbab52ea27cc2ad8fe
Ethmane Didi Bilal <blueskytech@Ethmanes-MacBook-Pro.local>
2024-08-14 13:30:52 +0200

    Optimize assets and add EAS build configurations

---
+++ app.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  name: "Mindful Flow",
+  slug: "mindful-flow",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.yourname.mindfulflow",
+  },
+  android: {
+    package: "com.yourname.mindfulflow",
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#FFFFFF",
+    },
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+};
