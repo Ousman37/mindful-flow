9eec3d08026223058106dc341a66ee8b0939b614
Ethmane Didi Bilal <blueskytech@Ethmanes-MacBook-Pro.local>
2024-08-15 11:46:03 +0200

    Add android package identifier to app.config.js

--- app.config.js
+++ app.config.js
@@ -1,39 +1,35 @@
 module.exports = {
-  expo: {
-    name: "mindful-flow",
-    slug: "mindful-flow",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/images/icon.png",
-    scheme: "myapp",
-    userInterfaceStyle: "automatic",
-    splash: {
-      image: "./assets/mindFulFlowLogo.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff",
+  name: "Mindful Flow",
+  slug: "mindful-flow",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  splash: {
+    image: "./assets/images/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.yourname.mindfulflow",
+  },
+  android: {
+    package: "com.yourname.mindfulflow", // <-- Add this line
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#FFFFFF",
     },
-    ios: {
-      supportsTablet: true,
-    },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/images/adaptive-icon.png",
-        backgroundColor: "#ffffff",
-      },
-    },
-    web: {
-      bundler: "metro",
-      output: "static",
-      favicon: "./assets/images/favicon.png",
-    },
-    plugins: ["expo-router"],
-    experiments: {
-      typedRoutes: true,
-    },
-    extra: {
-      eas: {
-        projectId: "d879e6c8-0de5-48a5-bac5-7284804d03e2", // Ensure your EAS project ID is here
-      },
+  },
+  web: {
+    favicon: "./assets/images/favicon.png",
+  },
+  extra: {
+    eas: {
+      projectId: "d879e6c8-0de5-48a5-bac5-7284804d03e2",
     },
   },
 };
